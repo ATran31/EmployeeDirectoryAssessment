148fa7fdc5774386d0369a2b48dbfa31427b9f7e
An Tran <an.tran2131@gmail.com>
2023-04-15 10:56:39 -0400

    Add tests for new dept id route.

--- backend/test/departments.test.ts
+++ backend/test/departments.test.ts
@@ -1,19 +1,36 @@
-import { expect } from "chai";
-import { it, describe } from "mocha";
-import { agent as request } from "supertest";
+import { expect } from 'chai';
+import { it, describe } from 'mocha';
+import { agent as request } from 'supertest';
 
-import Application from "../src/app";
+import Application from '../src/app';
 const app = new Application();
 
-describe("#departments", () => {
-	it("Should return success", async () => {
-		const res = await request(app.service).get("/v1/departments").send();
+describe('#departments', () => {
+	it('All depts return success', async () => {
+		const res = await request(app.service).get('/v1/departments').send();
 		expect(res.status).to.equal(200);
 		expect(res.body).not.to.be.empty;
-		expect(res.body).to.be.a("array");
-		expect(res.body.length).to.equal(3);
-		expect(res.body[0].id).to.be.a("number");
-		expect(res.body[0].id).to.equal(2);
+		expect(res.body).to.be.a('array');
+		expect(res.body[0].id).to.be.a('number');
 		expect(res.body.error).to.be.undefined;
+	});
+	it('Invalid dept returns 404', async () => {
+		const res = await request(app.service)
+			.get('/v1/departments/wrong')
+			.send();
+		expect(res.status).to.equal(404);
+		expect(res.body).to.be.empty;
+	});
+	it('Valid dept returns one record', async () => {
+		const res = await request(app.service).get('/v1/departments/2').send();
+		expect(res.status).to.equal(200);
+		expect(res.body).to.not.be.empty;
+		expect(res.body).to.be.an('object');
+		expect(res.body).to.have.property('id');
+		expect(res.body.id).to.be.a('number');
+		expect(res.body).to.have.property('name');
+		expect(res.body.name).to.be.a('string');
+		expect(res.body).to.have.property('employeeIds');
+		expect(res.body.employeeIds).to.be.an('array');
 	});
 });
